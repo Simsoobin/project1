8bf57917bf2b9e53f2d407d1be9311563f440668
Simsoobin <55381009+Simsoobin@users.noreply.github.com>
2019-11-25 17:19:17 +0900

    12_5

--- main.c
+++ main.c
@@ -4,8 +4,26 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char str[30] = "happy C programming";
+	int j;
+	char input[100];
+	FILE *fp1;
+	FILE *fp2;
 	
-	printf( "%s : %i\n", str, strlen(str) );
+	fp1 = fopen("sample.txt", "w");
+	for(j = 0; j < 3; j++) {
+		printf("input a word : ");
+		scanf("%s", input);
+		fprintf(fp1, "%s\n", input);
+	}
+	fclose(fp1);
+	printf("\n");
+	
+	fp2 = fopen("sample.txt", "r");
+	for(j = 0; j < 3; j++) {
+		fscanf(fp2, "%s", input);
+		printf("%s\n", input);
+	}
+	fclose(fp2);
+	
 	return 0;
 }
